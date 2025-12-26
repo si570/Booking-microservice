fd58da36ac8147ad849303c9978fcc5da68f713e
si570 <111269867+si570@users.noreply.github.com>
2025-12-26 21:52:46 +0000

    Create BookingService.java

---
+++ BookingService.java
@@ -0,0 +1,32 @@
+package ie.atu.week2.project.Booking;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class BookingService {
+
+    private final BookingRepository bookingRepository;
+
+    public BookingService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public Booking createBooking(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+
+    public Booking updateStatus(Long id, BookingStatus status) {
+        Booking booking = bookingRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Booking not found"));
+        booking.setStatus(status);
+        return bookingRepository.save(booking);
+    }
+
+    public void deleteBooking(Long id) {
+        bookingRepository.deleteById(id);
+    }
+}
